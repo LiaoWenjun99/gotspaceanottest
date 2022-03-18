5fcbb4d968223917d77d5bcc2333718b940b8154
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-18 12:25:09 +0800

    Delete Libraryspace directory

--- Libraryspace/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LibraryspaceConfig(AppConfig):
-    name = 'Libraryspace'
--- Libraryspace/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.db import connection
-from django.http import HttpResponse
-
-# Create your views here.
-
-def home(request):
-    return HttpResponse('<h1>gotspaceanot?</h1>')
